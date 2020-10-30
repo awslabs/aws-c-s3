80e11fa72dae407d78d921dcc9c84a9d785f059e
Justin Boswell <boswej@amazon.com>
2020-10-30 21:49:33 +0000

    More fixes

--- benchmarks/lib/canary.sh
+++ benchmarks/lib/canary.sh
@@ -4,7 +4,7 @@ set -ex
 
 if [ ! -x /home/ec2-user/install/bin/aws-crt-cpp-canary ]; then
     cd /tmp
-    yum update
+    yum update -y
     yum install -y cmake3 git gcc
     git clone https://github.com/awslabs/aws-crt-cpp.git
     cd aws-crt-cpp
@@ -14,7 +14,7 @@ if [ ! -x /home/ec2-user/install/bin/aws-crt-cpp-canary ]; then
     curl -LO https://www.openssl.org/source/openssl-1.1.1-latest.tar.gz
     tar -xzvf openssl-1.1.1-latest.tar.gz
     cd `tar ztf openssl-1.1.1-latest.tar.gz | head -n1 | cut -f1 -d/`
-    ./config -fPIC no-shared no-md2 no-rc5 no-rfc3779 no-sctp no-ssl-trace no-zlib no-hw no-mdc2 no-seed no-idea enable-ec_nistp_64_gcc_128 no-camellia no-bf no-ripemd no-dsa no-ssl2 no-ssl3 no-capieng -DSSL_FORBID_ENULL -DOPENSSL_NO_DTLS1 -DOPENSSL_NO_HEARTBEATS -—prefix=/home/ec2-user/install
+    ./config -fPIC no-shared no-md2 no-rc5 no-rfc3779 no-sctp no-ssl-trace no-zlib no-hw no-mdc2 no-seed no-idea enable-ec_nistp_64_gcc_128 no-camellia no-bf no-ripemd no-dsa no-ssl2 no-ssl3 no-capieng -DSSL_FORBID_ENULL -DOPENSSL_NO_DTLS1 -DOPENSSL_NO_HEARTBEATS --prefix=/home/ec2-user/install
     make -j
     make install_sw
     mkdir -p /tmp/aws-crt-cpp
