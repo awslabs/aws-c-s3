fe3a64aa07318a7cc6d0a386ab909a20e08a7cf4
Justin Boswell <boswej@amazon.com>
2020-10-30 21:52:06 +0000

    Fixed build dir for cmake

--- benchmarks/lib/canary.sh
+++ benchmarks/lib/canary.sh
@@ -17,8 +17,8 @@ if [ ! -x /home/ec2-user/install/bin/aws-crt-cpp-canary ]; then
     ./config -fPIC no-shared no-md2 no-rc5 no-rfc3779 no-sctp no-ssl-trace no-zlib no-hw no-mdc2 no-seed no-idea enable-ec_nistp_64_gcc_128 no-camellia no-bf no-ripemd no-dsa no-ssl2 no-ssl3 no-capieng -DSSL_FORBID_ENULL -DOPENSSL_NO_DTLS1 -DOPENSSL_NO_HEARTBEATS --prefix=/home/ec2-user/install
     make -j
     make install_sw
-    mkdir -p /tmp/aws-crt-cpp
-    cd /tmp/aws-crt-cpp
+    mkdir -p /tmp/aws-crt-cpp/build
+    cd /tmp/aws-crt-cpp/build
     cmake3 .. -DCMAKE_BUILD_TYPE=Release -DBUILD_DEPS=true -DCMAKE_INSTALL_PREFIX=/home/ec2-user/install -DCMAKE_PREFIX_PATH=/home/ec2-user/install
     cmake3 --build --target install
 fi
