c0ab00ec40cacd6119937eef1924e1fa0aa8a89e
Ryan Carper <rccarper@amazon.com>
2020-11-13 09:52:32 -0500

    Merging aws_s3_request_desc into aws_s3_request

--- include/aws/s3/private/s3_meta_request_impl.h
+++ include/aws/s3/private/s3_meta_request_impl.h
@@ -260,11 +260,19 @@ struct aws_s3_request *aws_s3_request_new(
     uint32_t flags);
 
 /* Set up the request to be sent. Called each time before the request is sent. Will initially call
+<<<<<<< HEAD
  * aws_s3_request_clean_up_send_data to clear out anything previously existing in send_data. */
 void aws_s3_request_setup_send_data(struct aws_s3_request *request, struct aws_http_message *message);
 
 /* Clear out send_data members so that they can be repopulated before the next send. */
 void aws_s3_request_clean_up_send_data(struct aws_s3_request *request);
+=======
+ * aws_s3_request_clear_send_data to clear out anything previously existing in send_data. */
+void aws_s3_request_setup_send_data(struct aws_s3_request *request, struct aws_http_message *message);
+
+/* Clear out send_data members so that they can be repopulated before the next send. */
+void aws_s3_request_clear_send_data(struct aws_s3_request *request);
+>>>>>>> Merging aws_s3_request_desc into aws_s3_request
 
 void aws_s3_request_acquire(struct aws_s3_request *request);
 
