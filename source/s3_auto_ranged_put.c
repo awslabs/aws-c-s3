021128ec34c6e16c2f3f2779c4cd5907f929f3ba
Ryan Carper <rccarper@amazon.com>
2020-11-18 13:19:05 -0500

    Fixing funciton call

--- source/s3_auto_ranged_put.c
+++ source/s3_auto_ranged_put.c
@@ -683,7 +683,7 @@ static int s_s3_auto_ranged_put_stream_complete(
         s_s3_auto_ranged_put_unlock_synced_data(auto_ranged_put);
 
         /* Create Multipart Upload finished successfully, so now we should have parts to send. */
-        aws_s3_client_schedule_meta_request_work(meta_request->client, &auto_ranged_put->base);
+        aws_s3_meta_request_schedule_work(&auto_ranged_put->base);
 
     } else if (request->desc_data.request_tag == AWS_S3_AUTO_RANGED_PUT_REQUEST_TAG_PART) {
 
