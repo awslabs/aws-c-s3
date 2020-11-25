344736085538e5a46ff992c1291d148c38ebb707
Ryan Carper <rccarper@amazon.com>
2020-11-25 15:30:23 -0500

    Changing signing approach

--- include/aws/s3/private/s3_util.h
+++ include/aws/s3/private/s3_util.h
@@ -8,6 +8,7 @@
 
 /* This file provides access to useful constants and simple utility functions. */
 
+#include <aws/auth/signing_config.h>
 #include <aws/common/byte_buf.h>
 
 #define ASSERT_SYNCED_DATA_LOCK_HELD(object) AWS_ASSERT(aws_mutex_try_lock(&(object)->synced_data.lock) == AWS_OP_ERR)
@@ -33,6 +34,21 @@ extern const struct aws_byte_cursor g_content_length_header_name;
 extern const struct aws_byte_cursor g_accept_ranges_header_name;
 extern const struct aws_byte_cursor g_etag_header_name;
 extern const struct aws_byte_cursor g_post_method;
+
+struct aws_cached_signing_config_aws {
+    struct aws_allocator *allocator;
+    struct aws_string *service;
+    struct aws_string *region;
+    struct aws_string *signed_body_value;
+
+    struct aws_signing_config_aws config;
+};
+
+struct aws_cached_signing_config_aws *aws_cached_signing_config_new(
+    struct aws_allocator *allocator,
+    const struct aws_signing_config_aws *signing_config);
+
+void aws_cached_signing_config_destroy(struct aws_cached_signing_config_aws *cached_signing_config);
 
 /* Sets all headers specified for src on dest */
 void copy_http_headers(const struct aws_http_headers *src, struct aws_http_headers *dest);
