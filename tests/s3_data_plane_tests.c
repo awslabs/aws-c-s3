b68080c09126e1db967ec7c38b2cb04758bfeeaf
Dengke Tang <dengket@amazon.com>
2021-01-12 16:30:14 -0800

    TODO for the tests

--- tests/s3_data_plane_tests.c
+++ tests/s3_data_plane_tests.c
@@ -1078,6 +1078,8 @@ static int s_test_s3_cancel_multipart_upload_during_parts_upload(struct aws_allo
     return 0;
 }
 
+/* TODO: instead of random, we should have some efforts to base on the request we made to test the cancel can happen
+ * anytime by user. */
 AWS_TEST_CASE(test_s3_cancel_multipart_upload_random, s_test_s3_cancel_multipart_upload_random)
 static int s_test_s3_cancel_multipart_upload_random(struct aws_allocator *allocator, void *ctx) {
     (void)ctx;
