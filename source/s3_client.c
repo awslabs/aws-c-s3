05805419ef54b169a9000e15988f6ae786120002
Ryan Carper <rccarper@amazon.com>
2021-01-14 23:10:52 -0500

    Taking out temp print

--- source/s3_client.c
+++ source/s3_client.c
@@ -1266,15 +1266,9 @@ static void s_s3_client_process_work_default(struct aws_s3_client *client) {
 
     aws_linked_list_swap_contents(&meta_request_work_list, &client->synced_data.pending_meta_request_work);
     aws_linked_list_swap_contents(&vip_connections_updates, &client->synced_data.pending_vip_connection_updates);
-
-    if (aws_sub_u32_checked(
-            client->threaded_data.num_requests_in_flight,
-            client->synced_data.pending_request_count,
-            &client->threaded_data.num_requests_in_flight)) {
-        /* TODO log */
-        printf("this should be a log\n");
-    }
-    // client->threaded_data.num_requests_in_flight -= client->synced_data.pending_request_count;
+    
+    /* TODO aws_sub_u32_checked */
+    client->threaded_data.num_requests_in_flight -= client->synced_data.pending_request_count;
     client->synced_data.pending_request_count = 0;
 
     aws_s3_client_unlock_synced_data(client);
