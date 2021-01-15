a4b384edb39b6051c4a2f0501f9e1f5aaeee5625
Ryan Carper <rccarper@amazon.com>
2021-01-15 11:37:21 -0500

    Clang-format

--- include/aws/s3/private/s3_auto_ranged_put.h
+++ include/aws/s3/private/s3_auto_ranged_put.h
@@ -50,7 +50,7 @@ struct aws_s3_auto_ranged_put {
         struct aws_s3_meta_request_finish_options *cached_finish_options;
 
         bool create_multipart_upload_successful;
-        
+
     } synced_data;
 };
 
