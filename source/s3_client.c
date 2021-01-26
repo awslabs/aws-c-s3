48709358599bca43a87aa6c3ea0a0e74bde25ab3
Ryan Carper <rccarper@amazon.com>
2021-01-26 16:43:51 -0500

    Fixing fix

--- source/s3_client.c
+++ source/s3_client.c
@@ -1327,7 +1327,6 @@ static void s_s3_client_process_work_default(struct aws_s3_client *client) {
 
             if (request == NULL) {
                 s_s3_client_remove_meta_request_threaded(client, current_meta_request);
-                current_meta_request_index = 0;
                 current_meta_request = NULL;
             } else {
                 ++current_meta_request_index;
