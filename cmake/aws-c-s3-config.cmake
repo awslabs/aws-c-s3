d41ab2a757194c36a178c29dd89fd7ebec2f0355
Itamar Levy-Or <ilevyor@amazon.com>
2021-04-27 15:14:01 -0400

    checksum depend

--- cmake/aws-c-s3-config.cmake
+++ cmake/aws-c-s3-config.cmake
@@ -2,6 +2,7 @@ include(CMakeFindDependencyMacro)
 
 find_dependency(aws-c-auth)
 find_dependency(aws-c-http)
+find_dependency(aws-checksums)
 
 if (BUILD_SHARED_LIBS)
     include(${CMAKE_CURRENT_LIST_DIR}/shared/@PROJECT_NAME@-targets.cmake)
