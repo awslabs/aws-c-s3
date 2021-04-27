d82c3185e2d9e65c0734b3d2a124bca0255c97e9
Dengke Tang <dengket@amazon.com>
2021-04-26 15:12:10 -1000

    dashboard and benchmark probably need different name

--- benchmarks/bin/benchmarks.ts
+++ benchmarks/bin/benchmarks.ts
@@ -6,15 +6,13 @@ import { BenchmarksStack } from '../lib/benchmarks-stack';
 import { DashboardStack } from '../lib/dashboard-stack';
 
 const app = new cdk.App();
-let benchmarks_stack_name = app.node.tryGetContext('StackName') as string;
-let dashboard_stack_name = app.node.tryGetContext('StackName') as string;
+let base_stack_name = app.node.tryGetContext('StackName') as string;
+let benchmarks_stack_name = 'BenchmarksStack'
+let dashboard_stack_name = 'DashboardStack';
 
-if (benchmarks_stack_name == null) {
-    benchmarks_stack_name = 'BenchmarksStack';
-}
-
-if (dashboard_stack_name == null) {
-    dashboard_stack_name = 'DashboardStack';
+if (base_stack_name != null) {
+    benchmarks_stack_name = benchmarks_stack_name + '-' + base_stack_name;
+    dashboard_stack_name = dashboard_stack_name + '-' + base_stack_name;
 }
 
 new BenchmarksStack(app, 'BenchmarksStack', { stackName: benchmarks_stack_name, env: { region: process.env.CDK_DEFAULT_REGION, account: process.env.CDK_DEFAULT_ACCOUNT } });
