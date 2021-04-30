c36a79e8d0d544fe57cddb89c855369199f450ac
Dengke Tang <dengket@amazon.com>
2021-04-29 14:57:55 -1000

    try to delete the stack after the bench mark finishes

--- benchmarks/bin/benchmarks.ts
+++ benchmarks/bin/benchmarks.ts
@@ -6,7 +6,7 @@ import { BenchmarksStack } from '../lib/benchmarks-stack';
 import { DashboardStack } from '../lib/dashboard-stack';
 
 const app = new cdk.App();
-let base_stack_name = app.node.tryGetContext('StackName') as string;
+const base_stack_name = app.node.tryGetContext('StackName') as string;
 let benchmarks_stack_name = 'BenchmarksStack'
 let dashboard_stack_name = 'DashboardStack';
 
