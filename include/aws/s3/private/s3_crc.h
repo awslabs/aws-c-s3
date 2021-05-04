7ac3ed5f3f90ee483189b94b50a34c4d5fdb1af1
Itamar Levy-Or <ilevyor@amazon.com>
2021-05-04 14:44:12 -0400

    add exports

--- include/aws/s3/private/s3_crc.h
+++ include/aws/s3/private/s3_crc.h
@@ -1,10 +1,23 @@
 #ifndef AWS_S3_CRC_H
 #define AWS_S3_CRC_H
 
+/**
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+#include <aws/s3/exports.h>
+
 #define AWS_CRC_LEN 4
 
+AWS_EXTERN_C_BEGIN
+
+AWS_S3_API
 struct aws_hash *aws_hash_crc32_new(struct aws_allocator *allocator);
 
+AWS_S3_API
 struct aws_hash *aws_hash_crc32c_new(struct aws_allocator *allocator);
+
+AWS_EXTERN_C_END
 
 #endif /* AWS_S3_CRC_H */
