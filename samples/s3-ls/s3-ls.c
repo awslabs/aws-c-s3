bff2cbe747155b51765427b120ce6d5e0be9d178
Cesar Mello <melcesar@amazon.com>
2021-11-09 17:57:33 -0800

    Using command_line_parser from aws-c-common

--- samples/s3-ls/s3-ls.c
+++ samples/s3-ls/s3-ls.c
@@ -4,12 +4,14 @@
  */
 
 #include <aws/auth/credentials.h>
+#include <aws/common/command_line_parser.h>
 #include <aws/common/condition_variable.h>
 #include <aws/common/mutex.h>
 #include <aws/common/zero.h>
 #include <aws/io/channel_bootstrap.h>
 #include <aws/io/event_loop.h>
 #include <aws/io/logging.h>
+#include <aws/io/uri.h>
 #include <aws/s3/private/s3_file_system_support.h>
 #include <aws/s3/s3.h>
 #include <aws/s3/s3_client.h>
@@ -24,7 +26,86 @@ struct app_ctx {
     struct aws_condition_variable c_var;
     bool execution_completed;
     struct aws_signing_config_aws signing_config;
+    const char *region;
+    enum aws_log_level log_level;
+    struct aws_uri uri;
 };
+
+static void s_usage(int exit_code) {
+
+    fprintf(stderr, "usage: s3-ls [options] s3://{bucket}[/prefix]\n");
+    fprintf(stderr, " bucket: the S3 bucket to list objects\n");
+    fprintf(stderr, " prefix: the prefix to filter\n");
+    fprintf(stderr, "\n Options:\n\n");
+    fprintf(stderr, "  -r, --region: the AWS region where the bucket is located. Default is us-west-2.\n");
+    fprintf(stderr, "  -v, --verbose: ERROR|INFO|DEBUG|TRACE: log level to configure. Default is none.\n");
+    fprintf(stderr, "  -h, --help\n");
+    fprintf(stderr, "            Display this message and quit.\n");
+    exit(exit_code);
+}
+
+static struct aws_cli_option s_long_options[] = {
+    {"region", AWS_CLI_OPTIONS_REQUIRED_ARGUMENT, NULL, 'r'},
+    {"verbose", AWS_CLI_OPTIONS_REQUIRED_ARGUMENT, NULL, 'v'},
+    {"help", AWS_CLI_OPTIONS_NO_ARGUMENT, NULL, 'h'},
+    /* Per getopt(3) the last element of the array has to be filled with all zeros */
+    {NULL, AWS_CLI_OPTIONS_NO_ARGUMENT, NULL, 0},
+};
+
+static void s_parse_options(int argc, char **argv, struct app_ctx *ctx) {
+    while (true) {
+        int option_index = 0;
+        int c = aws_cli_getopt_long(argc, argv, "r:v:h", s_long_options, &option_index);
+        if (c == -1) {
+            break;
+        }
+
+        switch (c) {
+            case 0:
+                /* getopt_long() returns 0 if an option.flag is non-null */
+                break;
+            case 'r':
+                ctx->region = aws_cli_optarg;
+                break;
+            case 'v':
+                if (!strcmp(aws_cli_optarg, "TRACE")) {
+                    ctx->log_level = AWS_LL_TRACE;
+                } else if (!strcmp(aws_cli_optarg, "INFO")) {
+                    ctx->log_level = AWS_LL_INFO;
+                } else if (!strcmp(aws_cli_optarg, "DEBUG")) {
+                    ctx->log_level = AWS_LL_DEBUG;
+                } else if (!strcmp(aws_cli_optarg, "ERROR")) {
+                    ctx->log_level = AWS_LL_ERROR;
+                } else {
+                    fprintf(stderr, "unsupported log level %s.\n", aws_cli_optarg);
+                    s_usage(1);
+                }
+                break;
+            case 'h':
+                s_usage(0);
+                break;
+            default:
+                fprintf(stderr, "Unknown option\n");
+                s_usage(1);
+        }
+    }
+
+    if (aws_cli_optind < argc) {
+        struct aws_byte_cursor uri_cursor = aws_byte_cursor_from_c_str(argv[aws_cli_optind++]);
+
+        if (aws_uri_init_parse(&ctx->uri, ctx->allocator, &uri_cursor)) {
+            fprintf(
+                stderr,
+                "Failed to parse uri %s with error %s\n",
+                (char *)uri_cursor.ptr,
+                aws_error_debug_str(aws_last_error()));
+            s_usage(1);
+        };
+    } else {
+        fprintf(stderr, "A URI for the request must be supplied.\n");
+        s_usage(1);
+    }
+}
 
 /**
  * Predicate used to decide if the application is ready to exit.
@@ -75,12 +156,9 @@ void s_on_list_finished(struct aws_s3_paginator *paginator, int error_code, void
     aws_condition_variable_notify_one(&app_ctx->c_var);
 }
 
-/**
- * optionally called to enable logs.
- */
 void setup_logger(struct app_ctx *app_ctx) {
     struct aws_logger_standard_options logger_options = {
-        .level = AWS_LOG_LEVEL_INFO,
+        .level = app_ctx->log_level,
         .file = stderr,
     };
 
@@ -88,53 +166,7 @@ void setup_logger(struct app_ctx *app_ctx) {
     aws_logger_set(&app_ctx->logger);
 }
 
-/**
- * gets the bucket name and prefix from the command line parameter.
- */
-int get_bucket_and_prefix_from_command_line(
-    const char *input,
-    struct aws_byte_cursor *bucket,
-    struct aws_byte_cursor *prefix) {
-    AWS_PRECONDITION(input);
-    AWS_PRECONDITION(bucket);
-    AWS_PRECONDITION(prefix);
-
-    const char *delimiter_pos = strchr(input, '/');
-    if (delimiter_pos == NULL) {
-        bucket->ptr = (uint8_t *)input;
-        bucket->len = strlen(input);
-        prefix->ptr = NULL;
-        prefix->len = 0;
-    } else {
-        bucket->ptr = (uint8_t *)input;
-        bucket->len = delimiter_pos - input;
-        prefix->ptr = (uint8_t *)(delimiter_pos + 1);
-        prefix->len = strlen(input) - bucket->len - 1;
-    }
-
-    return 0;
-}
-
 int main(int argc, char *argv[]) {
-
-    if (argc < 3) {
-        printf("Usage: s3-ls {bucket} region\n");
-        printf("       s3-ls {bucket}/{prefix} {region}\n");
-        printf("\n");
-        printf("Example:");
-        printf("\n");
-        printf("       s3-ls bucket us-west-2\n");
-
-        return -1;
-    }
-
-    const char *input = argv[1];
-    struct aws_byte_cursor bucket;
-    struct aws_byte_cursor prefix;
-    get_bucket_and_prefix_from_command_line(input, &bucket, &prefix);
-
-    const char *region = argv[2];
-    const bool enable_logs = false;
 
     struct aws_allocator *allocator = aws_default_allocator();
     aws_s3_library_init(allocator);
@@ -145,7 +177,19 @@ int main(int argc, char *argv[]) {
     app_ctx.c_var = (struct aws_condition_variable)AWS_CONDITION_VARIABLE_INIT;
     aws_mutex_init(&app_ctx.mutex);
 
-    if (enable_logs) {
+    s_parse_options(argc, argv, &app_ctx);
+
+    struct aws_byte_cursor bucket = app_ctx.uri.host_name;
+    struct aws_byte_cursor prefix;
+    if (app_ctx.uri.path.len == 0 || (app_ctx.uri.path.len == 1 && app_ctx.uri.path.ptr[0] == '/')) {
+        prefix.len = 0;
+        prefix.ptr = NULL;
+    } else {
+        /* sets prefix only if uri path is specified and not root / */
+        prefix = app_ctx.uri.path;
+    }
+
+    if (app_ctx.log_level != AWS_LOG_LEVEL_NONE) {
         setup_logger(&app_ctx);
     }
 
@@ -179,14 +223,14 @@ int main(int argc, char *argv[]) {
 
     /* signing config */
     aws_s3_init_default_signing_config(
-        &app_ctx.signing_config, aws_byte_cursor_from_c_str(region), credentials_provider);
+        &app_ctx.signing_config, aws_byte_cursor_from_c_str(app_ctx.region), credentials_provider);
     app_ctx.signing_config.flags.use_double_uri_encode = false;
 
     /* s3 client */
     struct aws_s3_client_config client_config;
     AWS_ZERO_STRUCT(client_config);
     client_config.client_bootstrap = client_bootstrap;
-    client_config.region = aws_byte_cursor_from_c_str(region);
+    client_config.region = aws_byte_cursor_from_c_str(app_ctx.region);
     client_config.signing_config = &app_ctx.signing_config;
     struct aws_s3_client *client = aws_s3_client_new(allocator, &client_config);
 
@@ -198,7 +242,7 @@ int main(int argc, char *argv[]) {
     params.prefix = prefix;
 
     char endpoint[1024];
-    snprintf(endpoint, sizeof(endpoint), "s3.%s.amazonaws.com", region);
+    snprintf(endpoint, sizeof(endpoint), "s3.%s.amazonaws.com", app_ctx.region);
     params.endpoint = aws_byte_cursor_from_c_str(endpoint);
     params.user_data = &app_ctx;
     params.on_object = &s_on_object;
