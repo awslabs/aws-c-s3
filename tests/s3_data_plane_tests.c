ba3122945205a99517cf35a7bcdaf276d5fb9a41
Itamar Levy-Or <ilevyor@amazon.com>
2022-01-21 16:13:30 -0500

    init test once

--- tests/s3_data_plane_tests.c
+++ tests/s3_data_plane_tests.c
@@ -1547,27 +1547,28 @@ AWS_TEST_CASE(test_s3_put_object_less_than_part_size_fc, s_test_s3_put_object_le
 static int s_test_s3_put_object_less_than_part_size_fc(struct aws_allocator *allocator, void *ctx) {
     (void)ctx;
 
-    for (int alg = AWS_SCA_CRC32C; alg < AWS_SCA_MD5; alg++) {
-        struct aws_s3_tester tester;
-        ASSERT_SUCCESS(aws_s3_tester_init(allocator, &tester, true));
+    // for (int alg = AWS_SCA_CRC32C; alg < AWS_SCA_MD5; alg++) {
+    int alg = AWS_SCA_CRC32;
+    struct aws_s3_tester tester;
+    ASSERT_SUCCESS(aws_s3_tester_init(allocator, &tester, true));
 
-        struct aws_s3_client_config client_config = {.part_size = 20 * 1024 * 1024, .checksum_algorithm = alg};
+    struct aws_s3_client_config client_config = {.part_size = 20 * 1024 * 1024, .checksum_algorithm = alg};
 
-        ASSERT_SUCCESS(aws_s3_tester_bind_client(
-            &tester, &client_config, AWS_S3_TESTER_BIND_CLIENT_REGION | AWS_S3_TESTER_BIND_CLIENT_SIGNING));
+    ASSERT_SUCCESS(aws_s3_tester_bind_client(
+        &tester, &client_config, AWS_S3_TESTER_BIND_CLIENT_REGION | AWS_S3_TESTER_BIND_CLIENT_SIGNING));
 
-        struct aws_s3_client *client = aws_s3_client_new(allocator, &client_config);
+    struct aws_s3_client *client = aws_s3_client_new(allocator, &client_config);
 
-        ASSERT_TRUE(client != NULL);
+    ASSERT_TRUE(client != NULL);
 
-        ASSERT_SUCCESS(aws_s3_tester_send_put_object_meta_request(
-            &tester, client, 1, AWS_S3_TESTER_SEND_META_REQUEST_EXPECT_SUCCESS, NULL));
+    ASSERT_SUCCESS(aws_s3_tester_send_put_object_meta_request(
+        &tester, client, 1, AWS_S3_TESTER_SEND_META_REQUEST_EXPECT_SUCCESS, NULL));
 
-        aws_s3_client_release(client);
-        client = NULL;
+    aws_s3_client_release(client);
+    client = NULL;
 
-        aws_s3_tester_clean_up(&tester);
-    }
+    aws_s3_tester_clean_up(&tester);
+    // }
     return 0;
 }
 
@@ -1603,30 +1604,31 @@ AWS_TEST_CASE(test_s3_put_object_empty_object_fc, s_test_s3_put_object_empty_obj
 static int s_test_s3_put_object_empty_object_fc(struct aws_allocator *allocator, void *ctx) {
     (void)ctx;
 
-    for (int alg = AWS_SCA_CRC32C; alg < AWS_SCA_MD5; alg++) {
-        struct aws_s3_tester tester;
-        ASSERT_SUCCESS(aws_s3_tester_init(allocator, &tester, true));
+    // for (int alg = AWS_SCA_CRC32C; alg < AWS_SCA_MD5; alg++) {
+    int alg = AWS_SCA_CRC32;
+    struct aws_s3_tester tester;
+    ASSERT_SUCCESS(aws_s3_tester_init(allocator, &tester, true));
 
-        struct aws_s3_client_config client_config;
-        AWS_ZERO_STRUCT(client_config);
+    struct aws_s3_client_config client_config;
+    AWS_ZERO_STRUCT(client_config);
 
-        client_config.checksum_algorithm = alg;
+    client_config.checksum_algorithm = alg;
 
-        ASSERT_SUCCESS(aws_s3_tester_bind_client(
-            &tester, &client_config, AWS_S3_TESTER_BIND_CLIENT_REGION | AWS_S3_TESTER_BIND_CLIENT_SIGNING));
+    ASSERT_SUCCESS(aws_s3_tester_bind_client(
+        &tester, &client_config, AWS_S3_TESTER_BIND_CLIENT_REGION | AWS_S3_TESTER_BIND_CLIENT_SIGNING));
 
-        struct aws_s3_client *client = aws_s3_client_new(allocator, &client_config);
+    struct aws_s3_client *client = aws_s3_client_new(allocator, &client_config);
 
-        ASSERT_TRUE(client != NULL);
+    ASSERT_TRUE(client != NULL);
 
-        ASSERT_SUCCESS(aws_s3_tester_send_put_object_meta_request(
-            &tester, client, 0, AWS_S3_TESTER_SEND_META_REQUEST_EXPECT_SUCCESS, NULL));
+    ASSERT_SUCCESS(aws_s3_tester_send_put_object_meta_request(
+        &tester, client, 0, AWS_S3_TESTER_SEND_META_REQUEST_EXPECT_SUCCESS, NULL));
 
-        aws_s3_client_release(client);
-        client = NULL;
+    aws_s3_client_release(client);
+    client = NULL;
 
-        aws_s3_tester_clean_up(&tester);
-    }
+    aws_s3_tester_clean_up(&tester);
+    // }
     return 0;
 }
 
