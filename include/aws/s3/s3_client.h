2a37467ac350b101d34171aed9ebc462b892319d
Eric Ace <ericace@protonmail.com>
2022-02-17 18:14:02 -0500

    Support meta requests on HTTP and HTTPS with custom ports (#174)
    The purpose is to allow meta requests over both http and https, and also to support custom ports in addition to the 80/443 hard-codes.
    
    Co-authored-by: Dengke Tang <dengket@amazon.com>
    Co-authored-by: esace <esace@evoforge.org>

--- include/aws/s3/s3_client.h
+++ include/aws/s3/s3_client.h
@@ -23,6 +23,7 @@ struct aws_s3_client;
 struct aws_s3_request;
 struct aws_s3_meta_request;
 struct aws_s3_meta_request_result;
+struct aws_uri;
 
 /**
  * A Meta Request represents a group of generated requests that are being done on behalf of the
@@ -235,6 +236,14 @@ struct aws_s3_meta_request_options {
      * See `aws_s3_meta_request_progress_fn`
      */
     aws_s3_meta_request_progress_fn *progress_callback;
+
+    /**
+     * Optional.
+     * The endpoint for the meta request to connect to. If not set, then tls settings from client will
+     * determine the port, and host header from `message` will determine the host for the connection.
+     * Note: must match the host header from `message`
+     */
+    struct aws_uri *endpoint;
 };
 
 /* Result details of a meta request.
