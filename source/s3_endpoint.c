3540157b111648707bdde91d3ae416a2571941f2
Gerrit Renker <grenker@aurora.tech>
2022-08-23 06:47:03 -0400

    Revert "Fix race condition in aws_s3_client_endpoint_release"
    This reverts commit 5bd8d211163bb32215338ddfa8e2f9436ed814ee,
    since it introduces a deadlock.

--- source/s3_endpoint.c
+++ source/s3_endpoint.c
@@ -224,11 +224,11 @@ void aws_s3_client_endpoint_release(struct aws_s3_client *client, struct aws_s3_
         if (aws_atomic_load_int(&endpoint->ref_count.ref_count) == 1) {
             aws_hash_table_remove(&client->synced_data.endpoints, endpoint->host_name, NULL, NULL);
         }
-        aws_ref_count_release(&endpoint->ref_count);
-
         aws_s3_client_unlock_synced_data(client);
     }
     /* END CRITICAL SECTION */
+
+    aws_ref_count_release(&endpoint->ref_count);
 }
 
 static void s_s3_endpoint_ref_count_zero(void *user_data) {
