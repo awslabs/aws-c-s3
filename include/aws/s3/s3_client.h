5f38c0e61f67d2839143df5c9b80e2569a99184d
Michael Graeb <graebm@amazon.com>
2022-09-27 12:04:54 -0700

    Fix some comments that referred to wrong variable name

--- include/aws/s3/s3_client.h
+++ include/aws/s3/s3_client.h
@@ -266,7 +266,7 @@ struct aws_s3_client_config {
      *
      * If true, each meta request has a flow-control window that shrinks as
      * response body data is downloaded (headers do not affect the window).
-     * `initial_window_size` determines the starting size of each meta request's window.
+     * `initial_read_window` determines the starting size of each meta request's window.
      * You will stop downloading data whenever the flow-control window reaches 0
      * You must call aws_s3_meta_request_increment_read_window() to keep data flowing.
      *
@@ -431,9 +431,9 @@ struct aws_s3_meta_request *aws_s3_client_make_meta_request(
  * If the client was created with `enable_read_backpressure` set true,
  * each meta request has a flow-control window that shrinks as response
  * body data is downloaded (headers do not affect the size of the window).
- * The client's `initial_window_size` determines the starting size of each meta request's window.
+ * The client's `initial_read_window` determines the starting size of each meta request's window.
  * If a meta request's flow-control window reaches 0, no further data will be downloaded.
- * If the `initial_window_size` is 0, the request will not start until the window is incremented.
+ * If the `initial_read_window` is 0, the request will not start until the window is incremented.
  * Maintain a larger window to keep up a high download throughput,
  * parts cannot download in parallel unless the window is large enough to hold multiple parts.
  * Maintain a smaller window to limit the amount of data buffered in memory.
