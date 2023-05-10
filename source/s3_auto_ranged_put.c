f6d07d34c9b86b1f5cb6f4cc532bd8abe3242053
Dmitriy Musatkin <musatkd@amazon.com>
2023-05-10 16:26:52 -0700

    fix log

--- source/s3_auto_ranged_put.c
+++ source/s3_auto_ranged_put.c
@@ -329,7 +329,7 @@ static void s_s3_meta_request_auto_ranged_put_destroy(struct aws_s3_meta_request
     aws_s3_paginated_operation_release(auto_ranged_put->synced_data.list_parts_operation);
 
         AWS_LOGF_ERROR(
-            AWS_LS_S3_META_REQUEST, "Number of parts %u number of etags %u",
+            AWS_LS_S3_META_REQUEST, "Number of parts %lu number of etags %lu",
             aws_array_list_length(&auto_ranged_put->synced_data.etag_list),
             auto_ranged_put->synced_data.num_parts_completed
             );
