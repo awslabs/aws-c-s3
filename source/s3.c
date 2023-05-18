8c4666d390cfe0ffb1cf66253b0b5d6cce69e921
Dmitriy Musatkin <63878209+DmitriyMusatkin@users.noreply.github.com>
2023-05-18 16:29:06 -0700

    Update source/s3.c
    Co-authored-by: Michael Graeb <graebm@amazon.com>

--- source/s3.c
+++ source/s3.c
@@ -37,7 +37,7 @@ static struct aws_error_info s_errors[] = {
     AWS_DEFINE_ERROR_INFO_S3(AWS_ERROR_S3_OBJECT_MODIFIED, "The object modifed during download."),
     AWS_DEFINE_ERROR_INFO_S3(AWS_ERROR_S3_NON_RECOVERABLE_ASYNC_ERROR, "Async error received from S3 and not recoverable from retry."),
     AWS_DEFINE_ERROR_INFO_S3(AWS_ERROR_S3_METRIC_DATA_NOT_AVAILABLE, "The metric data is not available, the requests ends before the metric happens."),
-    AWS_DEFINE_ERROR_INFO_S3(AWS_ERROR_S3_INVALID_CONTENT_LENGTH, "Request has unexpected content length.")
+    AWS_DEFINE_ERROR_INFO_S3(AWS_ERROR_S3_INCORRECT_CONTENT_LENGTH_HEADER, "Request body length must match Content-Length header."),
 };
 /* clang-format on */
 
