ff4b702293180e4cb18a04f3dcc94b61f8f0f4f8
Dmitriy Musatkin <musatkd@amazon.com>
2023-05-18 16:44:49 -0700

    update docs

--- source/s3_auto_ranged_put.c
+++ source/s3_auto_ranged_put.c
@@ -525,9 +525,7 @@ static bool s_s3_auto_ranged_put_update(
             }
 
             /* There is one more request to send after all the parts (the complete-multipart-upload) but it can't be
-             * done until all the parts have been completed.
-             * TODO: slightly different handling right now between known and
-             * unknown content length. can this be unified?*/
+             * done until all the parts have been completed.*/
             if (auto_ranged_put->has_content_length) {
                 if (auto_ranged_put->synced_data.num_parts_completed != auto_ranged_put->synced_data.total_num_parts) {
                     goto has_work_remaining;
@@ -999,10 +997,17 @@ static int s_s3_auto_ranged_put_prepare_request(
         }
         case AWS_S3_AUTO_RANGED_PUT_REQUEST_TAG_COMPLETE_MULTIPART_UPLOAD: {
 
+            /* Note: completeMPU fails if no parts are provided. We could
+            * workaround it by uploading an empty part at the cost of
+            * complicating flow logic for dealing with noop parts, but that
+            * arguably adds a lot of complexity for little benefit.
+            * Pre-buffering parts to determine whether mpu is needed will
+            * resolve this issue.
+            */
             if (!auto_ranged_put->has_content_length && auto_ranged_put->prepare_data.num_parts_read_from_stream == 0) {
                 AWS_LOGF_ERROR(
                     AWS_LS_S3_META_REQUEST,
-                    "id=%p Could not create complete multipart upload for meta request with zero content length.",
+                    "id=%p 0 byte meta requests without Content-Length header are currently not supported. Set Content-Length header to 0 to upload empty object",
                     (void *)meta_request);
 
                 aws_raise_error(AWS_ERROR_S3_INVALID_CONTENT_LENGTH);
