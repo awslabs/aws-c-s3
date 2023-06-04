f0048ac11647a578577b587d83545fbd2ce174f5
Waqar Ahmed Khan <waahm7@gmail.com>
2023-06-04 16:34:23 -0700

    add script

---
+++ scripts/update_s3_endpoint_ruleset.py
@@ -0,0 +1,79 @@
+import json
+import boto3
+from botocore.exceptions import NoCredentialsError
+
+def escape_char(c):
+    if c == '\\':
+        return '\\\\'
+    elif c == '\'':
+        return '\\\''
+    elif c == '\a':
+        return '\\a'
+    elif c == '\b':
+        return '\\b'
+    elif c == '\f':
+        return '\\f'
+    elif c == '\n':
+        return '\\n'
+    elif c == '\r':
+        return '\\r'
+    elif c == '\t':
+        return '\\t'
+    elif c == '\v':
+        return '\\v'
+    else:
+        return c
+
+
+def generate_c_file_from_json(s3, bucket_name, s3_file_name, c_file_name, c_struct_name):
+    num_chars_per_line = 20
+
+    try:
+        # Retrieve the JSON file from S3
+        response = s3.get_object(Bucket=bucket_name, Key=s3_file_name)
+        json_content_str = response['Body'].read().decode()
+        json_content = json.loads(json_content_str)
+        # Compact the json
+        compact_json_str: str = json.dumps(json_content, separators=(',', ':'))
+
+        # Write json to a C file
+        with open(c_file_name, 'w') as f:
+            f.write(
+                "/**\n * Copyright Amazon.com, Inc. or its affiliates.\n * All Rights Reserved. SPDX-License-Identifier: Apache-2.0.\n */\n\n")
+            f.write('#include <aws/s3/s3_endpoint_resolver.h>\n\n')
+            f.write('/**\n * This file is generated using a Python script.\n * Do not modify directly. */\n')
+            f.write('/* clang-format off */\n')
+            f.write(f"const char {c_struct_name}[] = {{\n\t")
+            for i in range(0, len(compact_json_str), num_chars_per_line):
+                f.write(', '.join("'{}'".format(escape_char(char)) for char in compact_json_str[i:i + num_chars_per_line]))
+                if i + 15 < len(compact_json_str):
+                    f.write(",\n\t")
+            f.write(", '\\0'};\n")
+
+        print(f"{c_file_name} has been created successfully.")
+
+    except NoCredentialsError:
+        print("No AWS credentials found.")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+if __name__ == '__main__':
+    session = boto3.session.Session()
+    s3 = session.client('s3', region_name='us-east-1')
+    bucket_name = 'aws-c-s3-models'
+
+    generate_c_file_from_json(
+        s3,
+        bucket_name,
+        's3-endpoint-rule-set.json',
+        'source/s3_endpoint_resolver/aws_s3_endpoint_rule_set.c',
+        'aws_s3_endpoint_rule_set')
+
+    generate_c_file_from_json(
+        s3,
+        bucket_name,
+        'partitions.json',
+        'source/s3_endpoint_resolver/aws_s3_endpoint_resolver_partition.c',
+        'aws_s3_endpoint_resolver_partitions')
