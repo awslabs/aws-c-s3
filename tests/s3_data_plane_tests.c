8c8a5bba531cddae8089cfc401aaf3f63cbf9a83
Dengke Tang <dengket@amazon.com>
2023-10-06 11:34:10 -0700

    add two more test with unknown content length set or not

--- tests/s3_data_plane_tests.c
+++ tests/s3_data_plane_tests.c
@@ -3678,9 +3678,11 @@ static int s_test_s3_round_trip_mpu_default_get_fc(struct aws_allocator *allocat
     return 0;
 }
 
-AWS_TEST_CASE(test_s3_round_trip_with_filepath, s_test_s3_round_trip_with_filepath)
-static int s_test_s3_round_trip_with_filepath(struct aws_allocator *allocator, void *ctx) {
-    (void)ctx;
+static int s_test_s3_round_trip_with_filepath_helper(
+    struct aws_allocator *allocator,
+    struct aws_byte_cursor key,
+    int object_size_mb,
+    bool unknown_content_length) {
 
     struct aws_s3_tester tester;
     ASSERT_SUCCESS(aws_s3_tester_init(allocator, &tester));
@@ -3700,8 +3702,7 @@ static int s_test_s3_round_trip_with_filepath(struct aws_allocator *allocator, v
     struct aws_byte_buf path_buf;
     AWS_ZERO_STRUCT(path_buf);
 
-    ASSERT_SUCCESS(aws_s3_tester_upload_file_path_init(
-        allocator, &path_buf, aws_byte_cursor_from_c_str("/prefix/round_trip/with_filepath")));
+    ASSERT_SUCCESS(aws_s3_tester_upload_file_path_init(allocator, &path_buf, key));
 
     struct aws_byte_cursor object_path = aws_byte_cursor_from_buf(&path_buf);
 
@@ -3714,7 +3715,7 @@ static int s_test_s3_round_trip_with_filepath(struct aws_allocator *allocator, v
                 .object_size_mb = 1,
                 .object_path_override = object_path,
                 .file_on_disk = true,
-                .skip_content_length = true,
+                .skip_content_length = unknown_content_length,
             },
     };
 
@@ -3746,75 +3747,43 @@ static int s_test_s3_round_trip_with_filepath(struct aws_allocator *allocator, v
     return 0;
 }
 
+AWS_TEST_CASE(test_s3_round_trip_with_filepath, s_test_s3_round_trip_with_filepath)
+static int s_test_s3_round_trip_with_filepath(struct aws_allocator *allocator, void *ctx) {
+    (void)ctx;
+    return s_test_s3_round_trip_with_filepath_helper(
+        allocator, aws_byte_cursor_from_c_str("/prefix/round_trip/with_filepath"), 1, false /*unknown_content_length*/);
+}
+
 AWS_TEST_CASE(test_s3_round_trip_mpu_with_filepath, s_test_s3_round_trip_mpu_with_filepath)
 static int s_test_s3_round_trip_mpu_with_filepath(struct aws_allocator *allocator, void *ctx) {
     (void)ctx;
+    return s_test_s3_round_trip_with_filepath_helper(
+        allocator,
+        aws_byte_cursor_from_c_str("/prefix/round_trip/with_filepath_mpu"),
+        50,
+        false /*unknown_content_length*/);
+}
 
-    struct aws_s3_tester tester;
-    ASSERT_SUCCESS(aws_s3_tester_init(allocator, &tester));
+AWS_TEST_CASE(test_s3_round_trip_with_filepath_no_content_length, s_test_s3_round_trip_with_filepath_no_content_length)
+static int s_test_s3_round_trip_with_filepath_no_content_length(struct aws_allocator *allocator, void *ctx) {
+    (void)ctx;
+    return s_test_s3_round_trip_with_filepath_helper(
+        allocator,
+        aws_byte_cursor_from_c_str("/prefix/round_trip/with_filepath_no_content_length"),
+        1,
+        true /*unknown_content_length*/);
+}
 
-    struct aws_s3_tester_client_options client_options = {
-        .part_size = MB_TO_BYTES(8),
-    };
-
-    struct aws_s3_client *client = NULL;
-    ASSERT_SUCCESS(aws_s3_tester_client_new(&tester, &client_options, &client));
-
-    struct aws_s3_meta_request_test_results test_results;
-    aws_s3_meta_request_test_results_init(&test_results, allocator);
-
-    /*** PUT FILE ***/
-
-    struct aws_byte_buf path_buf;
-    AWS_ZERO_STRUCT(path_buf);
-
-    ASSERT_SUCCESS(aws_s3_tester_upload_file_path_init(
-        allocator, &path_buf, aws_byte_cursor_from_c_str("/prefix/round_trip/with_filepath_mpu")));
-
-    struct aws_byte_cursor object_path = aws_byte_cursor_from_buf(&path_buf);
-
-    struct aws_s3_tester_meta_request_options put_options = {
-        .allocator = allocator,
-        .meta_request_type = AWS_S3_META_REQUEST_TYPE_PUT_OBJECT,
-        .checksum_algorithm = AWS_SCA_CRC32,
-        .client = client,
-        .put_options =
-            {
-                .object_size_mb = 50,
-                .object_path_override = object_path,
-                .file_on_disk = true,
-                .skip_content_length = true,
-            },
-    };
-
-    ASSERT_SUCCESS(aws_s3_tester_send_meta_request_with_options(&tester, &put_options, NULL));
-
-    /*** GET FILE ***/
-    aws_s3_meta_request_test_results_clean_up(&test_results);
-    aws_s3_meta_request_test_results_init(&test_results, allocator);
-
-    struct aws_s3_tester_meta_request_options get_options = {
-        .allocator = allocator,
-        .meta_request_type = AWS_S3_META_REQUEST_TYPE_GET_OBJECT,
-        .validate_type = AWS_S3_TESTER_VALIDATE_TYPE_EXPECT_SUCCESS,
-        .expected_validate_checksum_alg = AWS_SCA_CRC32,
-        .validate_get_response_checksum = true,
-        .client = client,
-        .get_options =
-            {
-                .object_path = object_path,
-            },
-    };
-    ASSERT_SUCCESS(aws_s3_tester_send_meta_request_with_options(&tester, &get_options, &test_results));
-
-    ASSERT_UINT_EQUALS(MB_TO_BYTES(put_options.put_options.object_size_mb), test_results.received_body_size);
-
-    aws_s3_meta_request_test_results_clean_up(&test_results);
-    aws_byte_buf_clean_up(&path_buf);
-    aws_s3_client_release(client);
-    aws_s3_tester_clean_up(&tester);
-
-    return 0;
+AWS_TEST_CASE(
+    test_s3_round_trip_mpu_with_filepath_no_content_length,
+    s_test_s3_round_trip_mpu_with_filepath_no_content_length)
+static int s_test_s3_round_trip_mpu_with_filepath_no_content_length(struct aws_allocator *allocator, void *ctx) {
+    (void)ctx;
+    return s_test_s3_round_trip_with_filepath_helper(
+        allocator,
+        aws_byte_cursor_from_c_str("/prefix/round_trip/with_filepath_mpu_no_content_length"),
+        50,
+        true /*unknown_content_length*/);
 }
 
 AWS_TEST_CASE(test_s3_chunked_then_unchunked, s_test_s3_chunked_then_unchunked)
