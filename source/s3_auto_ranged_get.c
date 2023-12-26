1e77d702207c9345cdb7bcb7c37086e1a37ef5f8
Waqar Ahmed Khan <waahm7@gmail.com>
2023-12-26 13:08:04 -0800

    Update source/s3_auto_ranged_get.c
    Co-authored-by: Michael Graeb <graebm@amazon.com>

--- source/s3_auto_ranged_get.c
+++ source/s3_auto_ranged_get.c
@@ -151,7 +151,7 @@ static enum aws_s3_auto_ranged_get_request_type s_s3_get_request_type_for_discov
     /*
      * If a range header exists but has no start-range (i.e. Range: bytes=-100), we perform a HeadRequest. If the
      * start-range is unknown, we could potentially execute a request from the end-range and keep that request around
-     * until the meta request finishes. However, this approach involves the complexity of managing back pressure. For
+     * until the meta request finishes. However, this approach involves the complexity of managing backpressure. For
      * simplicity, we execute a HeadRequest if the start-range is not specified.
      */
     if (auto_ranged_get->initial_message_has_range_header != 0) {
