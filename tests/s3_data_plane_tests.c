9ca78cf5faf8a793446a77b6bb1b60e4eb273f99
Waqar Ahmed Khan <waahm7@gmail.com>
2023-12-26 14:32:41 -0800

    Add test for empty file with range case

--- tests/s3_data_plane_tests.c
+++ tests/s3_data_plane_tests.c
@@ -5829,6 +5829,48 @@ static int s_test_s3_invalid_start_range_greator_than_end_range(struct aws_alloc
     return 0;
 }
 
+AWS_TEST_CASE(test_s3_invalid_empty_file_with_range, s_test_s3_invalid_empty_file_with_range)
+static int s_test_s3_invalid_empty_file_with_range(struct aws_allocator *allocator, void *ctx) {
+    (void)ctx;
+
+    struct aws_s3_tester tester;
+    ASSERT_SUCCESS(aws_s3_tester_init(allocator, &tester));
+
+    struct aws_s3_tester_client_options client_options = {
+        .part_size = 16 * 1024,
+    };
+
+    struct aws_s3_client *client = NULL;
+    ASSERT_SUCCESS(aws_s3_tester_client_new(&tester, &client_options, &client));
+
+    struct aws_s3_tester_meta_request_options options = {
+        .allocator = allocator,
+        .client = client,
+        .meta_request_type = AWS_S3_META_REQUEST_TYPE_GET_OBJECT,
+        .validate_type = AWS_S3_TESTER_VALIDATE_TYPE_EXPECT_FAILURE,
+        .get_options =
+            {
+                .object_path = g_pre_existing_empty_object,
+                .object_range = AWS_BYTE_CUR_INIT_FROM_STRING_LITERAL("bytes=0-0"),
+            },
+    };
+
+    struct aws_s3_meta_request_test_results results;
+    aws_s3_meta_request_test_results_init(&results, allocator);
+
+    ASSERT_SUCCESS(aws_s3_tester_send_meta_request_with_options(&tester, &options, &results));
+    ASSERT_INT_EQUALS(AWS_HTTP_STATUS_CODE_416_REQUESTED_RANGE_NOT_SATISFIABLE, results.finished_response_status);
+    ASSERT_NOT_NULL(results.error_response_operation_name);
+    ASSERT_TRUE(aws_string_eq_c_str(results.error_response_operation_name, "GetObject"));
+
+    aws_s3_meta_request_test_results_clean_up(&results);
+
+    aws_s3_client_release(client);
+    aws_s3_tester_clean_up(&tester);
+
+    return 0;
+}
+
 static const struct aws_byte_cursor g_x_amz_copy_source_name =
     AWS_BYTE_CUR_INIT_FROM_STRING_LITERAL("x-amz-copy-source");
 
