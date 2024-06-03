47cf8e5c03e6a3db855a5d4e104ebb07f9d3a492
Zhihui Xia <zhvxia@amazon.com>
2024-06-03 16:12:43 -0700

    setup builder for clang3

--- source/s3_copy_object.c
+++ source/s3_copy_object.c
@@ -544,7 +544,7 @@ static struct aws_string *s_etag_new_from_upload_part_copy_response(
     struct aws_byte_buf *response_body) {
 
     struct aws_byte_cursor xml_doc = aws_byte_cursor_from_buf(response_body);
-    struct aws_byte_cursor etag_within_xml_quotes = {0};
+    struct aws_byte_cursor etag_within_xml_quotes = {0, 0};
     const char *xml_path[] = {"CopyPartResult", "ETag", NULL};
     aws_xml_get_body_at_path(allocator, xml_doc, xml_path, &etag_within_xml_quotes);
 
@@ -666,7 +666,7 @@ static void s_s3_copy_object_request_finished(
                 struct aws_byte_cursor xml_doc = aws_byte_cursor_from_buf(&request->send_data.response_body);
 
                 /* Find the upload id for this multipart upload. */
-                struct aws_byte_cursor upload_id = {0};
+                struct aws_byte_cursor upload_id = {0, 0};
                 const char *xml_path[] = {"InitiateMultipartUploadResult", "UploadId", NULL};
                 aws_xml_get_body_at_path(meta_request->allocator, xml_doc, xml_path, &upload_id);
 
@@ -754,7 +754,7 @@ static void s_s3_copy_object_request_finished(
                 struct aws_byte_cursor xml_doc = aws_byte_cursor_from_buf(&request->send_data.response_body);
 
                 /* Grab the ETag for the entire object, and set it as a header. */
-                struct aws_byte_cursor etag_header_value = {0};
+                struct aws_byte_cursor etag_header_value = {0, 0};
                 const char *xml_path[] = {"CompleteMultipartUploadResult", "ETag", NULL};
                 aws_xml_get_body_at_path(meta_request->allocator, xml_doc, xml_path, &etag_header_value);
                 if (etag_header_value.len > 0) {
