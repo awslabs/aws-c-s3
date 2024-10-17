56e921e7db0144b50d1bce7350ef1992924802fc
Dengke <dengket@amazon.com>
2024-10-17 15:57:27 -0700

    hmmmm

--- tests/s3_tester.c
+++ tests/s3_tester.c
@@ -2380,7 +2380,11 @@ int aws_test_s3_copy_object_from_x_amz_copy_source(
     struct aws_s3_client_config client_config;
     AWS_ZERO_STRUCT(client_config);
     client_config.enable_s3express = s3express;
-    struct aws_byte_cursor region_cursor = aws_byte_cursor_from_c_str("us-west-2");
+
+    struct aws_byte_cursor region_cursor = g_test_s3_region;
+    if (s3express) {
+        region_cursor = aws_byte_cursor_from_c_str("us-west-2");
+    }
     client_config.region = region_cursor;
     ASSERT_SUCCESS(aws_s3_tester_bind_client(&tester, &client_config, AWS_S3_TESTER_BIND_CLIENT_SIGNING));
 
