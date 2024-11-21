fcc2617be75a68fe147aa07289dfc15d2e6cbb7f
Dengke <dengket@amazon.com>
2024-11-21 10:29:53 -0800

    share the code

--- tests/s3_data_plane_tests.c
+++ tests/s3_data_plane_tests.c
@@ -3010,6 +3010,35 @@ static int s_test_s3_put_object_async_fail_reading(struct aws_allocator *allocat
     return 0;
 }
 
+static int s_test_validate_if_none_match_failure_response(struct aws_s3_meta_request_test_results *test_results) {
+
+    /**
+     * response body should be like:
+     * <Error>
+     * <Code>PreconditionFailed</Code>
+     * <Message>At least one of the pre-conditions you specified did not hold</Message>
+     * <Condition>If-None-Match</Condition>
+     * <RequestId></RequestId>
+     * <HostId></HostId>
+     * </Error>
+     */
+    ASSERT_UINT_EQUALS(AWS_HTTP_STATUS_CODE_412_PRECONDITION_FAILED, test_results->finished_response_status);
+
+    struct aws_byte_cursor xml_doc = aws_byte_cursor_from_buf(&test_results->error_response_body);
+    struct aws_byte_cursor error_code_string = {0};
+    struct aws_byte_cursor condition_string = {0};
+
+    const char *error_code_path[] = {"Error", "Code", NULL};
+    ASSERT_SUCCESS(aws_xml_get_body_at_path(test_results->allocator, xml_doc, error_code_path, &error_code_string));
+    ASSERT_TRUE(aws_byte_cursor_eq_c_str(&error_code_string, "PreconditionFailed"));
+
+    const char *condition_path[] = {"Error", "Condition", NULL};
+    ASSERT_SUCCESS(aws_xml_get_body_at_path(test_results->allocator, xml_doc, condition_path, &condition_string));
+    ASSERT_TRUE(aws_byte_cursor_eq_c_str(&condition_string, "If-None-Match"));
+
+    return AWS_OP_SUCCESS;
+}
+
 AWS_TEST_CASE(test_s3_put_object_if_none_match, s_test_s3_put_object_if_none_match)
 static int s_test_s3_put_object_if_none_match(struct aws_allocator *allocator, void *ctx) {
     (void)ctx;
@@ -3030,24 +3059,7 @@ static int s_test_s3_put_object_if_none_match(struct aws_allocator *allocator, v
             },
     };
     ASSERT_SUCCESS(aws_s3_tester_send_meta_request_with_options(NULL, &put_options, &test_results));
-
-    /**
-     * response body should be like:
-     * <Error>
-     * <Code>PreconditionFailed</Code>
-     * <Message>At least one of the pre-conditions you specified did not hold</Message>
-     * <Condition>If-None-Match</Condition>
-     * <RequestId></RequestId>
-     * <HostId></HostId>
-     * </Error>
-     */
-    ASSERT_UINT_EQUALS(AWS_HTTP_STATUS_CODE_412_PRECONDITION_FAILED, test_results.finished_response_status);
-
-    struct aws_byte_cursor xml_doc = aws_byte_cursor_from_buf(&test_results.error_response_body);
-    struct aws_byte_cursor condition_string = {0};
-    const char *xml_path[] = {"Error", "Condition", NULL};
-    ASSERT_SUCCESS(aws_xml_get_body_at_path(allocator, xml_doc, xml_path, &condition_string));
-    ASSERT_TRUE(aws_byte_cursor_eq_c_str(&condition_string, "If-None-Match"));
+    ASSERT_SUCCESS(s_test_validate_if_none_match_failure_response(&test_results));
 
     aws_s3_meta_request_test_results_clean_up(&test_results);
     return 0;
@@ -3073,24 +3085,7 @@ static int s_test_s3_put_object_mpu_if_none_match(struct aws_allocator *allocato
             },
     };
     ASSERT_SUCCESS(aws_s3_tester_send_meta_request_with_options(NULL, &put_options, &test_results));
-
-    /**
-     * response body should be like:
-     * <Error>
-     * <Code>PreconditionFailed</Code>
-     * <Message>At least one of the pre-conditions you specified did not hold</Message>
-     * <Condition>If-None-Match</Condition>
-     * <RequestId></RequestId>
-     * <HostId></HostId>
-     * </Error>
-     */
-    ASSERT_UINT_EQUALS(AWS_HTTP_STATUS_CODE_412_PRECONDITION_FAILED, test_results.finished_response_status);
-
-    struct aws_byte_cursor xml_doc = aws_byte_cursor_from_buf(&test_results.error_response_body);
-    struct aws_byte_cursor condition_string = {0};
-    const char *xml_path[] = {"Error", "Condition", NULL};
-    ASSERT_SUCCESS(aws_xml_get_body_at_path(allocator, xml_doc, xml_path, &condition_string));
-    ASSERT_TRUE(aws_byte_cursor_eq_c_str(&condition_string, "If-None-Match"));
+    ASSERT_SUCCESS(s_test_validate_if_none_match_failure_response(&test_results));
 
     aws_s3_meta_request_test_results_clean_up(&test_results);
     return 0;
