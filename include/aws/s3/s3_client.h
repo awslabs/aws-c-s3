c7975d2270ff14954f11e95b22cee8e151a109d6
Waqar Ahmed Khan <waahm7@gmail.com>
2025-01-24 10:12:00 -0800

    lint

--- include/aws/s3/s3_client.h
+++ include/aws/s3/s3_client.h
@@ -554,7 +554,8 @@ struct aws_s3_client_config {
      * If not set, client will create a default S3 Express provider under the hood.
      *
      * NOTE: THE FOLLOWING BEHAVIOR IS EXPERIMENTAL AND UNSTABLE
-     * Default S3 Express provider will pass the headers allowed in `g_s3_create_session_allowed_headers` to the CreateSession call.
+     * Default S3 Express provider will pass the headers allowed in `g_s3_create_session_allowed_headers` to the
+     * CreateSession call.
      */
     aws_s3express_provider_factory_fn *s3express_provider_override_factory;
     void *factory_user_data;
