895ba3d1db4bd2345d89d788c8443235d2368c05
DmitriyMusatkin <musatkd@amazon.com>
2025-06-24 10:34:02 -0700

    lint

--- include/aws/s3/s3_buffer_pool.h
+++ include/aws/s3/s3_buffer_pool.h
@@ -141,7 +141,7 @@ struct aws_s3_buffer_pool_config {
  * by logging failure reason, returning null and raising aws_error.
  */
 typedef struct aws_s3_buffer_pool *(aws_s3_buffer_pool_factory_fn)(struct aws_allocator *allocator,
-                                                                   struct aws_s3_buffer_pool_config config, 
+                                                                   struct aws_s3_buffer_pool_config config,
                                                                    void *user_data);
 
 AWS_EXTERN_C_END
