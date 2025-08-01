c27e8295b433090433b4eb1e041083c0d5e2635e
Dmitriy Musatkin <63878209+DmitriyMusatkin@users.noreply.github.com>
2025-08-01 10:17:06 -0700

    change stale issue and discussion handling to run once a week (#541)

--- source/s3_checksum_stream.c
+++ source/s3_checksum_stream.c
@@ -35,7 +35,19 @@ static int s_finalize_checksum(struct aws_checksum_stream *impl) {
         return aws_raise_error(AWS_ERROR_S3_CHECKSUM_CALCULATION_FAILED);
     }
     struct aws_byte_cursor checksum_result_cursor = aws_byte_cursor_from_buf(&impl->checksum_result);
-    AWS_FATAL_ASSERT(aws_base64_encode(&checksum_result_cursor, impl->encoded_checksum_output) == AWS_OP_SUCCESS);
+    if (aws_base64_encode(&checksum_result_cursor, impl->encoded_checksum_output) != AWS_OP_SUCCESS) {
+        AWS_LOGF_ERROR(
+            AWS_LS_S3_CLIENT,
+            "Failed to base64 encode checksum with error code %d (%s). Output capacity: %zu length %zu",
+            aws_last_error(),
+            aws_error_str(aws_last_error()),
+            impl->encoded_checksum_output->capacity,
+            impl->encoded_checksum_output->len);
+        aws_byte_buf_reset(&impl->checksum_result, true);
+        impl->checksum_finalized = true;
+        return aws_raise_error(AWS_ERROR_S3_CHECKSUM_CALCULATION_FAILED);
+    }
+
     impl->checksum_finalized = true;
     return AWS_OP_SUCCESS;
 }
